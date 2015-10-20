2b6ac8f1ae94eaafd4a13010832308d337918f6a
Douglas Hagan <dhagan@yahoo.com>
2015-10-19 16:18:21 -1000

    initial checkin

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name supcalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the supcalcApp
+ */
+angular.module('supcalcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name supcalcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the supcalcApp
+ */
+angular.module('supcalcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
